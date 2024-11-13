9e0a3f80c09db475ba1572a41a52117492da0e0f
Alex Silva <alex@mtcsoft.com.br>
2024-11-13 13:05:27 -0300

    Adicionar arquivos de projeto.

---
+++ ProgramacaoReativaADO/Models/Pedido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramacaoReativaADO.Models
+{
+    class Pedido
+    {
+        public int Id { get; set; }
+        public string Produto { get; set; }
+        public double Quantidade { get; set; }
+        public DateTime DataCriacao { get; set; }
+    }
+}
